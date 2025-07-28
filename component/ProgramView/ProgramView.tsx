3f36a65cc7a09bdfcac54dcac37d6a0f2e010188
StevenCodeStack <steven.codestack@gmail.com>
2025-07-28 09:56:53 +0800

    Redesign ProgramView

--- component/ProgramView/ProgramView.tsx
+++ component/ProgramView/ProgramView.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Subtitle from "../UI/Subtitle";
 import Title from "../UI/Title";
+import Informatika from "@/public/images/informatika.jpg";
+import SistemInformasi from "@/public/images/sistem_informasi.jpg";
+import SistemInformasiAkuntansi from "@/public/images/sistem_informasi_akuntansi.jpg";
+import Image from "next/image";
 
 const programs = [
-  "S1 Informatika",
-  "S1 Sistem Informasi",
-  "S1 Informasi Akuntansi",
+  { id: 1, content: "S1 Informasi", image: Informatika },
+  { id: 2, content: "S1 Sistem Informasi", image: SistemInformasi },
+  { id: 3, content: "S1 Informasi Akuntansi", image: SistemInformasiAkuntansi },
 ];
 
 const ProgramView = () => {
@@ -16,11 +20,18 @@ const ProgramView = () => {
 
       <div className="mt-14 grid grid-cols-1 gap-6 lg:grid-cols-3">
         {programs.map((e) => (
-          <div
-            key={e}
-            className="flex h-40 items-center justify-center bg-gradient-to-b from-[#1e066b] to-[#24010b] text-2xl text-white"
-          >
-            {e}
+          <div key={e.id} className="h-90 rounded-2xl bg-white p-6">
+            <div className="max-h=[80%] relative aspect-[4/3]">
+              <Image
+                src={e.image}
+                alt=""
+                fill
+                className="rounded-2xl object-cover"
+              />
+            </div>
+            <p className="font-manrope mt-3 text-center text-lg font-semibold">
+              {e.content}
+            </p>
           </div>
         ))}
       </div>
